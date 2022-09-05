d7df49ce0411198f2379a03563e6f13f29c710a0
Kluscan <92493522+Kluscan@users.noreply.github.com>
2022-09-04 22:06:44 -0300

    Incluíndo novo método para valores menores.

--- projetoAvalidador-ModeloTeste/src/main/java/org/iftm/entities/Avaliador.java
+++ projetoAvalidador-ModeloTeste/src/main/java/org/iftm/entities/Avaliador.java
@@ -1,14 +1,21 @@
 package org.iftm.entities;
 public class Avaliador {
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+	private double menorDeTodos = Double.POSITIVE_INFINITY;
 	public void avalia(Leilao leilao) {
 		for(Lance lance : leilao.getLances()) {
 			if(lance.getValor() > maiorDeTodos) {
 				maiorDeTodos = lance.getValor();
+			}else if(lance.getValor()< menorDeTodos) {
+				menorDeTodos = lance.getValor();
 			}
 		}
 	}
 	public double getMaiorLance() {
 		return maiorDeTodos;
+	}
+	
+	public double getMenorLance() {
+		return menorDeTodos;
 	}
 }
