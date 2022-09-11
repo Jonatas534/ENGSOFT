34a96d4ee0821b6c1676d17334db60f6b5b2e3fe
Kluscan <92493522+Kluscan@users.noreply.github.com>
2022-09-11 14:06:04 -0300

    Alteração em acrécismos de novos métodos

--- projetoAvalidador-ModeloTeste/src/main/java/org/iftm/entities/Avaliador.java
+++ projetoAvalidador-ModeloTeste/src/main/java/org/iftm/entities/Avaliador.java
@@ -6,7 +6,7 @@ public class Avaliador {
 		for(Lance lance : leilao.getLances()) {
 			if(lance.getValor() > maiorDeTodos) {
 				maiorDeTodos = lance.getValor();
-			}else if(lance.getValor()< menorDeTodos) {
+			} if(lance.getValor()< menorDeTodos) {
 				menorDeTodos = lance.getValor();
 			}
 		}
--- projetoAvalidador-ModeloTeste/src/test/java/org/iftm/entities/AvaliadorTest.java
+++ projetoAvalidador-ModeloTeste/src/test/java/org/iftm/entities/AvaliadorTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class AvaliadorTest {
 
 	@Test
-	public void testarMaiorLanceComOrdem() {
+	public void testarMenorLanceSemOrdem() {
 		Usuario joao = new Usuario("João");
 		Usuario jose = new Usuario("Jose");
 		Usuario maria = new Usuario("Maria");
@@ -16,10 +16,35 @@ class AvaliadorTest {
 		Avaliador avaliador = new Avaliador();
 		
 		//Criar lances
-		leiao.propoe(new Lance(maria, 250));
-		leiao.propoe(new Lance(joao, 300));
-		leiao.propoe(new Lance(jose, 400));
-
+		;
+		leiao.propoe(new Lance(joao, 300.0));
+		leiao.propoe(new Lance(jose, 400.0));
+		leiao.propoe(new Lance(maria, 250.0));
+		
+		
+		//Execução
+		avaliador.avalia(leiao);
+		
+		//Comparações
+		
+		Double valorEsperado = 250.0;
+		Assertions.assertEquals(valorEsperado,avaliador.getMenorLance(),0.0001);
+			
+	}
+	
+	@Test
+	public void testarMaiorLanceSemOrdem() {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+		Leilao leiao = new Leilao("Plastaytion 3 novo");
+		Avaliador avaliador = new Avaliador();
+		
+		//Criar lances
+		;
+		leiao.propoe(new Lance(joao, 300.0));
+		leiao.propoe(new Lance(jose, 400.0));
+		leiao.propoe(new Lance(maria, 250.0));
 		
 		
 		//Execução
@@ -29,10 +54,184 @@ class AvaliadorTest {
 		
 		Double valorEsperado = 400.0;
 		Assertions.assertEquals(valorEsperado,avaliador.getMaiorLance(),0.0001);
-		
-		
-		
-		
-	}
+	}		
+			
 
+	@Test
+	public void testarMenorLanceComOrdemCrescente() {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+		Leilao leiao = new Leilao("Plastaytion 3 novo");
+		Avaliador avaliador = new Avaliador();
+		
+		//Criar lances
+		
+		leiao.propoe(new Lance(maria, 250.0));
+		leiao.propoe(new Lance(joao, 300.0));
+		leiao.propoe(new Lance(jose, 400.0));
+	
+		
+		
+		//Execução
+		avaliador.avalia(leiao);
+		
+		//Comparações
+		
+		Double valorEsperado = 250.0;
+		Assertions.assertEquals(valorEsperado,avaliador.getMenorLance(),0.0001);
+	}
+	
+	
+
+	
+	@Test
+	public void testarMaiorLanceComOrdemCrescente() {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+		Leilao leiao = new Leilao("Plastaytion 3 novo");
+		Avaliador avaliador = new Avaliador();
+		
+		//Criar lances
+		leiao.propoe(new Lance(maria, 250.0));
+		leiao.propoe(new Lance(joao, 300.0));
+		leiao.propoe(new Lance(jose, 400.0));
+		
+		
+		
+		//Execução
+		avaliador.avalia(leiao);
+		
+		//Comparações
+		
+		Double valorEsperado = 400.0;
+		Assertions.assertEquals(valorEsperado,avaliador.getMaiorLance(),0.0001);
+			
+	}
+	
+	@Test
+	public void testarMenorLanceComOrdemDecrescente() {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+		Leilao leiao = new Leilao("Plastaytion 3 novo");
+		Avaliador avaliador = new Avaliador();
+		
+		//Criar lances
+		
+		leiao.propoe(new Lance(jose, 400.0));
+		leiao.propoe(new Lance(joao, 300.0));
+		leiao.propoe(new Lance(maria, 250.0));
+	
+		
+		
+		//Execução
+		avaliador.avalia(leiao);
+		
+		//Comparações
+		
+		Double valorEsperado = 250.0;
+		Assertions.assertEquals(valorEsperado,avaliador.getMenorLance(),0.0001);
+	}
+	
+	
+
+	
+	@Test
+	public void testarMaiorLanceComOrdemDecrescente() {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+		Leilao leiao = new Leilao("Plastaytion 3 novo");
+		Avaliador avaliador = new Avaliador();
+		
+		//Criar lances
+		leiao.propoe(new Lance(jose, 400.0));
+		leiao.propoe(new Lance(joao, 300.0));
+		leiao.propoe(new Lance(maria, 250.0));
+		
+		
+		
+		
+		//Execução
+		avaliador.avalia(leiao);
+		
+		//Comparações
+		
+		Double valorEsperado = 400.0;
+		Assertions.assertEquals(valorEsperado,avaliador.getMaiorLance(),0.0001);
+			
+	}
+	
+	@Test
+	public void testarMenorLanceComUmLance() {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+		Leilao leiao = new Leilao("Plastaytion 3 novo");
+		Avaliador avaliador = new Avaliador();
+		
+		//Criar lances
+		
+		
+		leiao.propoe(new Lance(maria, 250.0));
+	
+		
+		
+		//Execução
+		avaliador.avalia(leiao);
+		
+		//Comparações
+		
+		Double valorEsperado = 250.0;
+		Assertions.assertEquals(valorEsperado,avaliador.getMenorLance(),0.0001);
+	}
+	
+	@Test
+	public void testarMaiorLanceSemLance() {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+		Leilao leiao = new Leilao("Plastaytion 3 novo");
+		Avaliador avaliador = new Avaliador();
+		
+		//Criar lances
+		
+		
+		
+		//Execução
+		avaliador.avalia(leiao);
+		
+		//Comparações
+		
+		Double valorEsperado = 400.0;
+		Assertions.assertEquals(valorEsperado,avaliador.getMaiorLance(),0.0001);
+			
+	}
+	
+	@Test
+	public void testarMenorLanceSemLance() {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+		Leilao leiao = new Leilao("Plastaytion 3 novo");
+		Avaliador avaliador = new Avaliador();
+		
+		//Criar lances
+		
+		
+	
+		
+		
+		//Execução
+		avaliador.avalia(leiao);
+		
+		//Comparações
+		
+		Double valorEsperado = 250.0;
+		Assertions.assertEquals(valorEsperado,avaliador.getMenorLance(),0.0001);
+	}
+	
+	
 }
